e3f1e0873396e03796f2fbf9df45d2666bca103b
Cj Oyales <carljustineoyales@gmail.com>
2020-02-12 14:24:12 +0800

    Updates

---
+++ precache-manifest.36507d2be0b9b2c70f16b43b725b9231.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e593c9967745c7b8a7749caee0bb7793",
+    "url": "/reactjs-portfolio/index.html"
+  },
+  {
+    "revision": "e7bf118ae3d67b925597",
+    "url": "/reactjs-portfolio/static/css/main.3c29bdca.chunk.css"
+  },
+  {
+    "revision": "515a680187bf335e6be9",
+    "url": "/reactjs-portfolio/static/js/2.3837eaf4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/reactjs-portfolio/static/js/2.3837eaf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7bf118ae3d67b925597",
+    "url": "/reactjs-portfolio/static/js/main.17b1fced.chunk.js"
+  },
+  {
+    "revision": "f382358ec38ddb85a0c5",
+    "url": "/reactjs-portfolio/static/js/runtime-main.e4c37b2a.js"
+  },
+  {
+    "revision": "797fcb939f878542fd19720965e752c6",
+    "url": "/reactjs-portfolio/static/media/0bb51489073907.5de9556cd8868.797fcb93.png"
+  },
+  {
+    "revision": "53c53a86a0e0db593b8bc39a55bf02f0",
+    "url": "/reactjs-portfolio/static/media/4b797d87629215.5dbe40a11017e.53c53a86.png"
+  },
+  {
+    "revision": "f3696f81e4dfec413869f438cb7df9bd",
+    "url": "/reactjs-portfolio/static/media/567b6589187143.5ded711a88526.f3696f81.jpg"
+  },
+  {
+    "revision": "f37196e10e6b0b2cfd803a52700c5104",
+    "url": "/reactjs-portfolio/static/media/about-image.f37196e1.jpg"
+  },
+  {
+    "revision": "e9bddc249748872e7e1b1435b2972783",
+    "url": "/reactjs-portfolio/static/media/f3eba587597025.5dbcd5b42adae.e9bddc24.png"
+  },
+  {
+    "revision": "56d3ddb072dc2c7a0b93f0ebb04bdd84",
+    "url": "/reactjs-portfolio/static/media/logo.56d3ddb0.svg"
+  }
+]);
